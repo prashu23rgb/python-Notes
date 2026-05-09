3fec0c71f580070f5bfd0d4b7c8ff009c833939f
prashu23rgb <priyanshuutiwari22@gmail.com>
2026-05-09 10:36:42 +0530

    day 01

---
+++ basic_01.py
@@ -0,0 +1,7 @@
+#lets start from printing otuput on the screen
+print("hello guys this is my github repository")
+# lets create a function to add two numbers fuction is a block of code which performs a specific task and can be reused in the program. we can define a function using the def keyword followed by the function name and parentheses. we can also pass parameters to the function which are used to perform the task. in this example we will create a function to add two numbers and return the result.
+def add(a,b):
+    return a+b
+print(add(2,3))
+
